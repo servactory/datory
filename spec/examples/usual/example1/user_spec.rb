c72cc30b7d9323a18293b5d91daf6fccc0ef3f0f
Anton <profox.rus@gmail.com>
2024-04-21 14:41:18 +0700

    Update user spec to check detailed response
    The user spec in example1 has been updated to match and check for a detailed response instead of just checking for presence. This includes validations for numerous attributes such as `firstname`, `lastname`, `email`, `login` details, and `addresses`. This change enhances the efficiency of our testing by checking for expected content in addition to mere existence.

--- spec/examples/usual/example1/user_spec.rb
+++ spec/examples/usual/example1/user_spec.rb
@@ -53,14 +53,42 @@ RSpec.describe Usual::Example1::User do
     end
 
     it do
-      puts
-      puts
-      puts :perform
-      puts perform.inspect
-      puts
-      puts
-
-      expect(perform).to be_present
+      expect(perform).to match(
+        {
+          id: be_present,
+          firstname: "John",
+          lastname: "Doe",
+          email: "johndoe@example.com",
+          birthDate: "1973-01-22",
+          login: {
+            uuid: "1a0eed01-9430-4d68-901f-c0d4c1c3bf22",
+            username: "johndoe",
+            password: "a25723600f7",
+            md5: "c1328472c5794a25723600f71c1b4586", # rubocop:disable Naming/VariableNumber
+            sha1: "35544a31cc19bd6520af116554873167117f4d94", # rubocop:disable Naming/VariableNumber
+            registered: "2023-01-10T10:03:20.022Z"
+          },
+          addresses: [
+            {
+              street: "123 Main Street",
+              suite: "Apt. 4",
+              city: "Anytown",
+              zipcode: "12345-6789",
+              geo: {
+                lat: "42.1234",
+                lng: "-71.2345"
+              }
+            }
+          ],
+          phone: "(555) 555-1234",
+          website: "www.johndoe.com",
+          company: {
+            name: "ABC Company",
+            catchPhrase: "Innovative solutions for all your needs",
+            bs: "Marketing"
+          }
+        }
+      )
     end
   end
 
