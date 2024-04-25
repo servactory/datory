c96f53edf25ff599feabc1bfa8cd74b4b49bc2ff
Anton <profox.rus@gmail.com>
2024-04-25 22:45:46 +0700

    Refactor service factory creation method
    The creation method in the service factory has been split into two parts for readability and maintainability. Unnecessary code for output model type assignment has been removed. This change simplifies the structure and optimizes method length and complexity.

--- lib/datory/attributes/tools/service_factory.rb
+++ lib/datory/attributes/tools/service_factory.rb
@@ -13,13 +13,18 @@ module Datory
           @collection_of_attributes = collection_of_attributes
         end
 
-        # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
         def create
           return if @model_class.const_defined?(ServiceBuilder::SERVICE_CLASS_NAME)
 
+          class_sample = create_service_class
+
+          @model_class.const_set(ServiceBuilder::SERVICE_CLASS_NAME, class_sample)
+        end
+
+        def create_service_class # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
           collection_of_attributes = @collection_of_attributes
 
-          class_sample = Class.new(Datory::Service::Builder) do
+          Class.new(Datory::Service::Builder) do
             collection_of_attributes.each do |attribute|
               input_internal_name = attribute.options.fetch(:to, attribute.name)
 
@@ -39,24 +44,8 @@ module Datory
                 outputs.public_send(:"#{input_internal_name}=", value)
               end
             end
-
-            # output :model, type: Class
-
-            # make :assign_model_name
-
-            # def assign_model_name
-            #   model_class_name_array = self.class.name.split("::")
-            #   model_class_name_array.pop
-            #   model_class_name = model_class_name_array.join("::")
-            #   model_class = model_class_name.constantize
-            #
-            #   outputs.model = model_class
-            # end
           end
-
-          @model_class.const_set(ServiceBuilder::SERVICE_CLASS_NAME, class_sample)
         end
-        # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
       end
     end
   end
