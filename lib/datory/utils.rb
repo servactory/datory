c1c380d8af248703ca55dccdeb930f065d3f2ef6
Anton <profox.rus@gmail.com>
2024-04-26 19:38:11 +0700

    Refactor transformation methods in utils.rb
    Removed the unused `_type` argument in the transformation methods in utils.rb. This simplifies the method invocations and improves code readability.

--- lib/datory/utils.rb
+++ lib/datory/utils.rb
@@ -18,14 +18,14 @@ module Datory
     private_constant :D_TRANSFORMATIONS
 
     S_TRANSFORMATIONS = {
-      Symbol => ->(value, _type) { value.to_sym },
-      String => ->(value, _type) { value.to_s },
-      Integer => ->(value, _type) { value.to_i },
-      Float => ->(value, _type) { value.to_f },
-      Date => ->(value, _type) { value.to_s },
-      Time => ->(value, _type) { value.to_s },
-      DateTime => ->(value, _type) { value.to_s },
-      ActiveSupport::Duration => ->(value, _type) { value.iso8601 }
+      Symbol => ->(value) { value.to_sym },
+      String => ->(value) { value.to_s },
+      Integer => ->(value) { value.to_i },
+      Float => ->(value) { value.to_f },
+      Date => ->(value) { value.to_s },
+      Time => ->(value) { value.to_s },
+      DateTime => ->(value) { value.to_s },
+      ActiveSupport::Duration => ->(value) { value.iso8601 }
     }.freeze
 
     private_constant :S_TRANSFORMATIONS
@@ -34,7 +34,7 @@ module Datory
       if format == :d
         D_TRANSFORMATIONS.fetch(type, ->(v) { v }).call(value)
       else
-        S_TRANSFORMATIONS.fetch(value.class, ->(v, _type) { v }).call(value, type)
+        S_TRANSFORMATIONS.fetch(value.class, ->(v) { v }).call(value)
       end
     end
   end
