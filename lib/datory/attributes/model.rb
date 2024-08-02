0443c1bc50fbc80d0523ec6c26054a1d363e6fdd
Anton <profox.rus@gmail.com>
2024-08-02 19:58:50 +0900

    Refactor attribute processing logic in model.rb
    Replaced use of each method on attributes with collection_of_attributes to retrieve attribute name and value. This change improves code readability and prepares for future handling of nested attribute hashes.

--- lib/datory/attributes/model.rb
+++ lib/datory/attributes/model.rb
@@ -7,29 +7,40 @@ module Datory
         new(...).build!
       end
 
-      def initialize(context, attributes, _collection_of_attributes)
+      def initialize(context, attributes, collection_of_attributes)
         @context = context
         @attributes = attributes
-        # @collection_of_attributes = collection_of_attributes
+        @collection_of_attributes = collection_of_attributes
       end
 
       def build!
-        @attributes.each do |attribute_name, attribute_value|
+        @collection_of_attributes.each do |attribute|
+          attribute_name = attribute.to.name
+          attribute_value = @attributes.fetch(attribute_name, nil)
+
           if attribute_value.is_a?(Hash)
-            # nested_attribute = @collection_of_attributes.find_by(name: attribute_name)
-            #
-            # nested_result =
-            #   if nested_attribute.present?
-            #     nested_attribute.to.include_class.new(**attribute_value)
-            #   else
-            #     attribute_value
-            #   end
-            #
-            # assign_attribute_for(nested_attribute.to.include_class, name: attribute_name, value: nested_result)
+            # TODO
           else
             assign_attribute_for(@context, name: attribute_name, value: attribute_value)
           end
         end
+
+        # @attributes.each do |attribute_name, attribute_value|
+        #   if attribute_value.is_a?(Hash)
+        #     # nested_attribute = @collection_of_attributes.find_by(name: attribute_name)
+        #     #
+        #     # nested_result =
+        #     #   if nested_attribute.present?
+        #     #     nested_attribute.to.include_class.new(**attribute_value)
+        #     #   else
+        #     #     attribute_value
+        #     #   end
+        #     #
+        #     # assign_attribute_for(nested_attribute.to.include_class, name: attribute_name, value: nested_result)
+        #   else
+        #     assign_attribute_for(@context, name: attribute_name, value: attribute_value)
+        #   end
+        # end
       end
 
       private
